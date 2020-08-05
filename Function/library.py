59b54b807e1d4c5a164a51172aa61107ef3b58a0
shekharbiswas <biswas.berlin89@gmail.com>
2020-08-05 22:10:06 +0200

    updated

--- Function/library.py
+++ Function/library.py
@@ -5,3 +5,11 @@ def unique_item(l1):
 def sort_list(l1):
     return l1.sort_list()
 
+def fib(n):
+    a, b = 0, 1
+	
+    while (a < n):
+        print(a, end=' ')
+        a = b
+        b = a + b
+    print()
