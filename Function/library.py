d83a10e38c76aeb77a41003fc11f2a8951536f1b
shekharbiswas <biswas.berlin89@gmail.com>
2020-08-05 21:02:34 +0200

    updated

--- Function/library.py
+++ Function/library.py
@@ -5,14 +5,3 @@ def unique_item(l1):
 def sort_list(l1):
     return l1.sort_list()
 
-class Polygon:
-    def __init__(self, no_of_sides):
-        self.n = no_of_sides
-        self.sides = [0 for i in range(no_of_sides)]
-
-    def inputSides(self):
-        self.sides = [float(input("Enter side "+str(i+1)+" : ")) for i in range(self.n)]
-
-    def dispSides(self):
-        for i in range(self.n):
-        print("Side",i+1,"is",self.sides[i])
